7befd9f58fe3a6b27c4dd83cf43d4631740fcf77
dzhangarnnn <70790352+dzhangarnnn@users.noreply.github.com>
2022-11-22 21:21:54 +0700

    Update bookmark.jsx

--- src/App/components/bookmark.jsx
+++ src/App/components/bookmark.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const BookMark = ({status, ...rest}) => {
     return (
-        <i class={"bi bi-bookmark" + (status ? "-heart" : '')} {...rest}></i>
+        <button {...rest}>
+            <i class={"bi bi-bookmark" + (status ? "-heart" : '')}></i>
+        </button>
     );
 };
 
