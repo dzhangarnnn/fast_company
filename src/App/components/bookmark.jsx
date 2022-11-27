2cb874ba5c2a475bb112976d774a11fa584e444e
dzhangarnnn <dzhangarnnn@gmail.com>
2022-11-27 13:23:42 +0700

    pagination comp created

--- src/App/components/bookmark.jsx
+++ src/App/components/bookmark.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const BookMark = ({status, ...rest}) => {
-    return (
-        <button {...rest}>
-            <i class={"bi bi-bookmark" + (status ? "-heart" : '')}></i>
-        </button>
-    );
+const BookMark = ({ status, ...rest }) => {
+	return (
+		<button {...rest}>
+			<i className={"bi bi-bookmark" + (status ? "-heart" : "")}></i>
+		</button>
+	);
 };
 
 export default BookMark;
--- src/App/components/qualitie.jsx
+++ src/App/components/qualitie.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
-const Qualitie = ({ color, name, _id }) => {
-    return (
-        <span 
-            className={"badge m-1 bg-" + color} 
-        >
-            {name}
-        </span>
-    )
+const Qualitie = ({ color, name }) => {
+	return <span className={"badge m-1 bg-" + color}>{name}</span>;
 };
 
 export default Qualitie;
