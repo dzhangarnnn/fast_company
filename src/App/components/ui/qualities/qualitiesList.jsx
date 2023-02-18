07070c188f8607469314ed44998ccbb74617af77
dzhangarnnn <dzhangarnnn@gmail.com>
2023-02-19 03:23:27 +0700

    qualities updated

--- src/App/components/ui/qualities/qualitiesList.jsx
+++ src/App/components/ui/qualities/qualitiesList.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Quality from "./quality";
+import { useQualities } from "../../../hooks/useQuality";
 
-const QualitiesList = ({ qualities }) => {
-    return (
-        <>
-            {qualities.map((qual) => (
-                <Quality key={qual._id} {...qual} />
-            ))}
-        </>
-    );
+const QualitiesList = ({ qualitiesIds }) => {
+    const { getQuality, isLoading } = useQualities();
+
+    if (!isLoading) {
+        return (
+            <>
+                {qualitiesIds.map((id) => (
+                    <Quality key={id} {...getQuality(id)} />
+                ))}
+            </>
+        );
+    } else return "...loading";
 };
 
 QualitiesList.propTypes = {
-    qualities: PropTypes.array
+    qualitiesIds: PropTypes.array
 };
 
 export default QualitiesList;
